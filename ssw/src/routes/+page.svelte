254947781db93d90764e538070ef9d9d41da5da2
xescr <114061880+xescr@users.noreply.github.com>
2023-07-21 19:04:52 +0800

    svelte beginning, radar chart

---
+++ ssw/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Radar from '$lib/Radar.svelte';
+</script>
+
+<div class="grid justify-center items-center w-screen h-screen">
+	<div class="bg-black w-12 h-12" />
+</div>
+
